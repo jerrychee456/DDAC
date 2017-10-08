1a9b79a9a4fcf50be1bb38365b4b39acfd389543
jerrychee456 <32621445+jerrychee456@users.noreply.github.com>
2017-10-09 05:02:15 +0800

    edit coonection string

--- DBConstants.php
+++ DBConstants.php
@@ -1,8 +1,8 @@
 <?php
 
 	define('DB_NAME', 'DDAC');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_HOST', 'localhost');
+	define('DB_USER', 'infinity@myddac');
+	define('DB_PASSWORD', 'Password123');
+	define('DB_HOST', 'myddac.mysql.database.azure.com');
 
 ?>
